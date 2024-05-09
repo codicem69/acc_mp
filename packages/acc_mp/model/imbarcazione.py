8639c4c2d630f84138b6a39fb6a4cde98909b359
tommaso <codicem69@gmail.com>
2024-05-09 15:18:32 +0200

    modifica size tipologia imb

--- packages/acc_mp/model/imbarcazione.py
+++ packages/acc_mp/model/imbarcazione.py
@@ -6,7 +6,7 @@ class Table(object):
         self.sysFields(tbl)
         tbl.column('cliente_id',size='22', group='_', name_long='!![it]Cliente id'
                     ).relation('cliente.id', relation_name='cliente_imb', mode='foreignkey', onDelete='cascade')
-        tbl.column('tip_id',size=':2', group='_', name_long='!![it]Tipologia'
+        tbl.column('tip_id',size=':3', group='_', name_long='!![it]Tipologia'
                     ).relation('tipologia_imb.code', relation_name='tip_imb', mode='foreignkey', onDelete='raise')
         tbl.column('nome', name_short='!![it]Nome')
         tbl.column('reg_n', name_short='!![en]Numero reg.')
