0293b6c9caa8601d6736c1ca47fefce1a907ccdc
tommaso <codicem69@gmail.com>
2024-05-13 10:04:22 +0200

    modifica stampa estartto corrispettivi + modifica menu

--- packages/acc_mp/menu.py
+++ packages/acc_mp/menu.py
@@ -1,9 +1,18 @@
 # encoding: utf-8
 class Menu(object):
     def config(self,root,**kwargs):
-        root.thpage(u"!![it]Cliente", table="acc_mp.cliente", multipage="True", tags="")
-        root.thpage(u"fat_emesse", table="acc_mp.fat_emesse", multipage="True", tags="")
-        root.thpage(u"!![it]Imbarcazioni", table="acc_mp.imbarcazione", multipage="True", tags="")
-        root.thpage(u"!![it]Pagamenti", table="acc_mp.pag_fat_emesse", multipage="True", tags="")
-        root.thpage(u"!![it]Estratto per corrispettivi", table="acc_mp.estratto_corrisp", multipage="True", tags="")
-        root.lookupBranch(u"Lookup tables", pkg="acc_mp")
+        taguser = self.db.currentEnv.get('userTags')
+        tag_user=taguser.split(',')
+
+        if 'admin' in tag_user or 'superadmin' in tag_user or '_DEV_' in tag_user:
+            root.packageBranch('Contabilità carburanti',pkg='acc_mp')
+            root.packageBranch('Amministrazione sistema',pkg='adm')
+            root.packageBranch('System',pkg='sys')
+
+        else:
+            root.thpage(u"!![it]Cliente", table="acc_mp.cliente", multipage="True", tags="")
+            root.thpage(u"fat_emesse", table="acc_mp.fat_emesse", multipage="True", tags="")
+            root.thpage(u"!![it]Imbarcazioni", table="acc_mp.imbarcazione", multipage="True", tags="")
+            #root.thpage(u"!![it]Pagamenti", table="acc_mp.pag_fat_emesse", multipage="True", tags="")
+            #root.thpage(u"!![it]Estratto per corrispettivi", table="acc_mp.estratto_corrisp", multipage="True", tags="")
+            root.lookupBranch(u"Lookup tables", pkg="acc_mp")
--- packages/acc_mp/resources/tables/fat_emesse/html_res/estratto_corrispettivi.py
+++ packages/acc_mp/resources/tables/fat_emesse/html_res/estratto_corrispettivi.py
@@ -13,7 +13,9 @@ class Main(TableScriptToHtml):
     doc_footer_height = 15
     doc_header_height = 16
     grid_row_height = 5
-    grid_header_height = 5
+    grid_header_height = 10
+    grid_style_cell='font-size: 12pt'
+    
     totalize_footer='Totale'
     cliente_height = 10
     #Fornendo a totalize_footer una stringa testuale, questa verrà usata come etichetta della riga di totalizzazione
@@ -45,7 +47,12 @@ class Main(TableScriptToHtml):
                             font-size:8pt;
                             }
                             """)
-
+    def gridLayout(self,body):
+        return body.layout(name='rowsL',um='mm',
+                            top=1,bottom=1,left=1,right=1,
+                            border_width=.3,
+                            style='line-height:5mm;text-align:center;font-size:12pt;')
+    
     def gridStruct(self,struct):
         #Questo metodo definisce la struttura della griglia di stampa definendone colonne e layout
         r = struct.view().rows()
@@ -53,7 +60,7 @@ class Main(TableScriptToHtml):
         r.cell('data', mm_width=15, name='Data')
          #r.fieldcell('mese_fattura', hidden=True, subtotal='Totale {breaker_value}', subtotal_order_by="$data")
          #Questa formulaColumn verrà utilizzata per creare i subtotali per mese
-        r.cell('doc_n', mm_width=0, name='Fattura n.')
+        r.cell('doc_n', mm_width=0, name='Fattura n.', content_class="breakword")
         #r.cell('doc_n', hidden=True, subtotal='Totale documento {breaker_value}',subtotal_order_by='$cliente')
         r.cell('sifiva', mm_width=30, name='Olio Naz./Sif+Iva/Bunker', totalize=True,format='#,###.00')
         r.cell('gasnaz', mm_width=20, name='Gasolio Naz.', totalize=True,format='#,###.00')
@@ -94,8 +101,10 @@ class Main(TableScriptToHtml):
             if r[3] == 'BENZ_8':
                 f_benz8 +=r[2] 
             if r[1]:
-                fat_n += r[1] + '-'    
-
+                fat_n += r[1] + '-'
+                if r == fat_emesse[-1]: 
+                    fat_n += r[1]
+                    
         report = self.db.table('diporto.report').query(columns="""$data,$venduto_gas, $venduto_benz""",
                                             where=where,
                                             data=data_report).fetch()
@@ -109,57 +118,25 @@ class Main(TableScriptToHtml):
 
         tot_giorn = f_sifiva + vend_benznaz + vend_gasnaz + f_sif + f_benz8
         righe.append(dict(data=self.parameter('date'),doc_n=fat_n, sifiva=f_sifiva, gasnaz=vend_gasnaz,benznaz=vend_benznaz,sif=f_sif,benz8=f_benz8,tot_estratto=tot_giorn))
-        #print(x)    
-            #print(x)
-
-        #for r in range(len(fat_emesse)):
-        #    fat_id=fat_emesse[r][7]
-        #    data_fat=fat_emesse[r][1]
-        #    doc_n=fat_emesse[r][2]
-        #    nome_imb=fat_emesse[r][3]
-        #    importo_fat=fat_emesse[r][4]
-        #    tot_pag=fat_emesse[r][5]
-        #    saldo=fat_emesse[r][6]
-        #    #insda=fat_emesse[r][7]
-        #    #if insda == True:
-        #    #    insda = 'x'
-        #    #    saldo_fat = 0
-        #    #else:
-        #    #    insda = ''
-        #    saldo_fat=importo_fat#
-        #    pag_progressivo=0#
-        #    for p in range(len(pagfatEmesse)):#
-        #        if pagfatEmesse[p][0] == fat_id:
-        #            data=pagfatEmesse[p][1]
-        #            tot_pag=pagfatEmesse[p][2]
-        #            descrizione_vers=pagfatEmesse[p][3]
-        #            if nome_imb is not None:
-        #                descrizione_vers='Importo versato - ' + str(nome_imb)
-        #            else:
-        #                descrizione_vers='Importo versato'   
-        #            
-        #            pag_progressivo += tot_pag
-        #            saldo_fat = 0
-        #            saldo_fat = importo_fat-pag_progressivo
-        #            #
-        #            righe_pag.append(dict(data=data,doc_n=doc_n, nome_imb=descrizione_vers, importo='',
-        #                      tot_pag=tot_pag,saldo='',cliente=cliente))
-        #    bal_cliente+=saldo_fat#
-        #    #if r == len(fat_emesse)-1:
-        #    #    righe_pag.append(dict(data='',doc_n='Balance', descrizione='Totale ' + str(cliente), importo='',
-        #    #                  tot_pag='',saldo='',cliente='',balance_cliente=bal_cliente))    #
-        #    righe_fat.append(dict(data=data_fat,doc_n=doc_n, nome_imb=nome_imb, importo=importo_fat,
-        #                      tot_pag='',saldo=saldo_fat,cliente=cliente))
-        #    
-        #    righe = []
-        #    righe_fat.extend(righe_pag)
-        #    for myDict in righe_fat:
-        #        if myDict not in righe:
-        #            righe.append(myDict)
-                
+        
         return righe    
     
+    def calcRowHeight(self):
+        #Determina l'altezza di ogni singola riga con approssimazione partendo dal valore di riferimento grid_row_height
+        docn_offset = 20
+        #Stabilisco un offset in termini di numero di caratteri oltre il quale stabilirò di andare a capo.
+        #Attenzione che in questo caso ho una dimensione in num. di caratteri, mentre la larghezza della colonna è definita
+        #in mm, e non avendo utti i caratteri la stessa dimensione si tratterà quindi di individuare la migliore approssimazione
+          
+        n_rows_docn = len(self.rowField('doc_n'))//docn_offset
 
+      
+  #      n_rows_nome_provincia = len(self.rowField('_sigla_provincia_nome'))//nome_offset + 1
+        #In caso di valori in relazione, è necessario utilizzare "_" nel metodo rowField per recuperare correttamente i valori
+        #A tal proposito si consiglia comunque sempre di utilizzare le aliasColumns
+        #n_rows = max(n_rows_docn)#, n_rows_nome_provincia)
+        height = (self.grid_row_height * n_rows_docn)
+        return height
     def docFooter(self, footer, lastPage=None):
         #Questo metodo definisce il layout e il contenuto dell'header della stampa
         foo = footer.layout('totali_fattura',top=1,
