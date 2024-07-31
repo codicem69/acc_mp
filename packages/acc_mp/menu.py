cc5f8dd863005e9301b12703dc76aafe741f99c2
tommaso <codicem69@gmail.com>
2024-07-31 18:01:35 +0200

    modifica menu

--- packages/acc_mp/menu.py
+++ packages/acc_mp/menu.py
@@ -1,12 +1,23 @@
 # encoding: utf-8
 class Menu(object):
     def config(self,root,**kwargs):
-
-            root.packageBranch('Amministrazione sistema',pkg='adm')
-            root.packageBranch('System',pkg='sys')
+        user=self.db.currentEnv.get('user')
+        taguser = self.db.currentEnv.get('userTags')
+        tag_user=taguser.split(',')
+        if 'admin' in tag_user or 'superadmin' in tag_user or '_DEV_' in tag_user:
+            accmp = root.branch(u"acc_mp", tags="")
+            accmp.packageBranch('Amministrazione sistema',pkg='adm')#, branchMethod='userSubmenu')
+            accmp.packageBranch('System',pkg='sys')
+            accmp.packageBranch('Agencies',pkg='agz')
+            accmp.thpage(u"!![it]Cliente", table="acc_mp.cliente", multipage="True", tags="")
+            accmp.thpage(u"fat_emesse", table="acc_mp.fat_emesse", multipage="True", tags="")
+            accmp.thpage(u"!![it]Imbarcazioni", table="acc_mp.imbarcazione", multipage="True", tags="")
+            accmp.thpage(u"!![it]Pagamenti", table="acc_mp.pag_fat_emesse", multipage="True", tags="")
+            accmp.lookupBranch(u"Lookup tables", pkg="acc_mp")
+        else:
+            #accmp = root.branch(u"acc_mp", tags="")
             root.thpage(u"!![it]Cliente", table="acc_mp.cliente", multipage="True", tags="")
             root.thpage(u"fat_emesse", table="acc_mp.fat_emesse", multipage="True", tags="")
             root.thpage(u"!![it]Imbarcazioni", table="acc_mp.imbarcazione", multipage="True", tags="")
-            root.thpage(u"!![it]Pagamenti", table="acc_mp.pag_fat_emesse", multipage="True", tags="")
-            #root.thpage(u"!![it]Estratto per corrispettivi", table="acc_mp.estratto_corrisp", multipage="True", tags="")
+            #accmp.thpage(u"!![it]Pagamenti", table="acc_mp.pag_fat_emesse", multipage="True", tags="")
             root.lookupBranch(u"Lookup tables", pkg="acc_mp")
